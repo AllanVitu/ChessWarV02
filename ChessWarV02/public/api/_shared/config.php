897c7b5a33ef0e901ac7e5e587ee27e43d198f3a
Kward <166002323+AllanVitu@users.noreply.github.com>
2026-01-10 14:24:04 +0100

    gggg

--- ChessWarV02/public/api/_shared/config.php
+++ ChessWarV02/public/api/_shared/config.php
@@ -5,6 +5,6 @@ return [
   'db_port' => '5432',
   'db_name' => 'vial4802_ChessWar',
   'db_user' => 'vial4802_Drawk',
-  'db_pass' => '@AllanStell@577!',
+  'db_pass' => 'sG4M-yNkP-MUr{',
   'db_sslmode' => '',
 ];

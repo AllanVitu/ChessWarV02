dffc758df12fed3d49cdce6575333d1e789634ab
Kward <166002323+AllanVitu@users.noreply.github.com>
2026-01-10 14:40:35 +0100

    GOOO STP

--- ChessWarV02/public/api/_shared/config.php
+++ ChessWarV02/public/api/_shared/config.php
@@ -4,7 +4,7 @@ return [
   'db_host' => 'localhost',
   'db_port' => '5432',
   'db_name' => 'vial4802_ChessWar',
-  'db_user' => 'vial4802_Drawk',
-  'db_pass' => 'sG4M-yNkP-MUr{',
+  'db_user' => 'vial4802',
+  'db_pass' => '@Stell@577!',
   'db_sslmode' => '',
 ];

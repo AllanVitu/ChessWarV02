aa46aef17f93f6a225b776635a9b9bf2bc721fde
Kward <166002323+AllanVitu@users.noreply.github.com>
2026-01-10 14:42:25 +0100

    GOOOOOOOOOOOOOOOOOOO

--- ChessWarV02/public/api/auth-register.php
+++ ChessWarV02/public/api/auth-register.php
@@ -1,8 +1,8 @@
 <?php
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+ini_set('log_errors', '1');
+ini_set('error_log', __DIR__ . '/_shared/error.log');
 error_reporting(E_ALL);
+
 
 
 

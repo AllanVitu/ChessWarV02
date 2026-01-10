1bb5bbc9188a5f0ef7e453791739c18d7913627d
Kward <166002323+AllanVitu@users.noreply.github.com>
2026-01-10 14:05:40 +0100

    go

--- ChessWarV02/public/api/auth-register.php
+++ ChessWarV02/public/api/auth-register.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+
 require_once __DIR__ . '/_shared/response.php';
 require_once __DIR__ . '/_shared/db.php';
 require_once __DIR__ . '/_shared/helpers.php';
